4fdd312cc3a38587fcb39e6c1e160db62432c695
Vinicius Emanuel <vinicius.emanuelds@gmail.com>
2024-07-22 17:45:06 -0300

    Desafio de sintaxe em Java e outras atualizações.

--- SINTAXE/src/terminal/AboutMe.java
+++ SINTAXE/src/terminal/AboutMe.java
@@ -1,9 +1,8 @@
 package terminal;
-
 import java.util.Locale;
 import java.util.Scanner;
 
-public class MinhaClasse {
+public class AboutMe {
     public static void main(String[] args) {
         //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
