4451c11b2e8a71452a428d97091eec787a6018d1
Vinicius Emanuel <vinicius.emanuelds@gmail.com>
2024-07-28 16:30:36 -0300

    Atualizaçãod de repositório

--- DESAFIOS/Spring/src/controller/ClienteRestController.java
+++ DESAFIOS/Spring/src/controller/ClienteRestController.java
@@ -1,3 +1,4 @@
+package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import one.digitalinnovation.gof.model.Cliente;
-import one.digitalinnovation.gof.service.ClienteService;
+import model.Cliente;
+import service.ClienteService;
 
 @RestController
 @RequestMapping("clientes")
